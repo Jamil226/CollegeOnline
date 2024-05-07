4c9493d3a042a9a49c91a6ae34c979992b9be4b2
Muhammad Jamil <jamil138.amin@gmail.com>
2024-05-07 10:20:24 +0500

    Toast Additon

--- app/src/main/java/com/mac/retrofit/Splash.java
+++ app/src/main/java/com/mac/retrofit/Splash.java
@@ -1,6 +1,8 @@
 package com.mac.retrofit;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,5 +13,6 @@ public class Splash extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        Toast.makeText(this, "Hello World!", Toast.LENGTH_SHORT).show();
     }
 }

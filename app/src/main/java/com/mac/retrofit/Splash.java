1bbe52c9867937365a627abf1cc8e0328ae9bdbd
Muhammad Jamil <jamil138.amin@gmail.com>
2024-05-07 10:16:23 +0500

    Initial commit

---
+++ app/src/main/java/com/mac/retrofit/Splash.java
@@ -0,0 +1,15 @@
+package com.mac.retrofit;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+    }
+}

566a3fee709257c140d36fcbaba7b6f7352ada4b
Muhammad Jamil <jamil138.amin@gmail.com>
2024-05-07 10:22:26 +0500

    Main Activity Added and Intent Added.

--- app/src/main/java/com/mac/retrofit/Splash.java
+++ app/src/main/java/com/mac/retrofit/Splash.java
@@ -1,5 +1,6 @@
 package com.mac.retrofit;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.Toast;
@@ -12,6 +13,9 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
 
         Toast.makeText(this, "Hello World!", Toast.LENGTH_SHORT).show();
     }
